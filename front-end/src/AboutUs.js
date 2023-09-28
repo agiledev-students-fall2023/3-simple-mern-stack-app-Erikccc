fdeda27499f03ff188379246774e5bc09443867a
Eric Zhu <lz2348@nyu.edu>
2023-09-27 22:35:58 -0400

    111

--- front-end/src/AboutUs.js
+++ front-end/src/AboutUs.js
@@ -6,7 +6,7 @@ const AboutUs = () => {
         <div>
             <h2>About Us</h2>
             <p>My name is Eric Zhu and I am a senior in NYU. My major is computer science joint econ and I have been struggled four years studying these two hard major.</p>
-            <img src={profilePic} alt="Eric Zhu" />
+            <img src={profilePic} alt="Eric Zhu" style={{ width: '150px', height: '150px' }} />
         </div>
     );
 }
