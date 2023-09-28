6ef8c455e87ad341ae1904f522c1d11414b8ede8
Eric Zhu <lz2348@nyu.edu>
2023-09-27 22:30:10 -0400

    111

--- front-end/src/AboutUs.js
+++ front-end/src/AboutUs.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const AboutUs = () => {
     return (
         <div>
-
-            
             <h2>About Us</h2>
             <p>My name is Eric Zhu and I am a senior in NYU. My major is computer science joint econ and I have been struggled four years studying these two hard major.</p>
-            {/* You can add more content, images, etc. */}
+            <img src="./WechatIMG232.jpg" />
         </div>
     );
 }
