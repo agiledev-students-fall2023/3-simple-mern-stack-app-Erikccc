83cc0ea8767cf065bc0dd7a1d223f6e40465b0e8
Eric Zhu <lz2348@nyu.edu>
2023-09-27 22:37:35 -0400

    111

--- front-end/src/AboutUs.js
+++ front-end/src/AboutUs.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import profilePic from './WechatIMG232.jpg'; // Adjust the path if needed
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const AboutUs = () => {
-    return (
-        <div>
-            <h2>About Us</h2>
-            <p>My name is Eric Zhu and I am a senior in NYU. My major is computer science joint econ and I have been struggled four years studying these two hard major.</p>
-            <img src={profilePic} alt="Eric Zhu" style={{ width: '150px', height: '150px' }} />
-        </div>
-    );
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    // Fetch data from backend
+    axios.get('/api/about-us')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  if (!data) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>{data.aboutText}</p>
+      <img src={data.imageUrl} alt="About Eric Zhu" style={{ width: '150px' }} />
+    </div>
+  );
 }
 
 export default AboutUs;
-
