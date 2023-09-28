5a0341e7c6a28f2dbc46c5767526aff7a16d2209
Eric Zhu <lz2348@nyu.edu>
2023-09-27 22:43:29 -0400

    111

--- back-end/app.js
+++ back-end/app.js
@@ -1,82 +1,87 @@
-require('dotenv').config({ silent: true }) // load environmental variables from a hidden file named .env
-const express = require('express') // CommonJS import style!
-const morgan = require('morgan') // middleware for nice logging of incoming HTTP requests
-const cors = require('cors') // middleware for enabling CORS (Cross-Origin Resource Sharing) requests.
-const mongoose = require('mongoose')
+require('dotenv').config({ silent: true });
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-const app = express() // instantiate an Express object
-app.use(morgan('dev', { skip: (req, res) => process.env.NODE_ENV === 'test' })) // log all incoming requests, except when in unit test mode.  morgan has a few logging default styles - dev is a nice concise color-coded style
-app.use(cors()) // allow cross-origin resource sharing
+const app = express();
+app.use(morgan('dev', { skip: (req, res) => process.env.NODE_ENV === 'test' }));
+app.use(cors());
 
-// use express's builtin body-parser middleware to parse any data included in a request
-app.use(express.json()) // decode JSON-formatted incoming POST data
-app.use(express.urlencoded({ extended: true })) // decode url-encoded incoming POST data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// connect to database
+// Serve static assets like images from the public directory
+app.use('/public', express.static('public'));
+
 mongoose
   .connect(`${process.env.DB_CONNECTION_STRING}`)
-  .then(data => console.log(`Connected to MongoDB`))
-  .catch(err => console.error(`Failed to connect to MongoDB: ${err}`))
+  .then(() => console.log(`Connected to MongoDB`))
+  .catch(err => console.error(`Failed to connect to MongoDB: ${err}`));
 
-// load the dataabase models we want to deal with
-const { Message } = require('./models/Message')
-const { User } = require('./models/User')
+const { Message } = require('./models/Message');
+const { User } = require('./models/User');
 
-// a route to handle fetching all messages
+// Sample data for "About Us" page
+const aboutUsData = {
+  aboutText: "My name is Eric Zhu and I am a senior in NYU. My major is computer science joint econ and I have been struggled four years studying these two hard major.",
+  imageUrl: "/public/WechatIMG232.jpg"
+};
+
+// Endpoint to serve "About Us" data
+app.get('/api/about-us', (req, res) => {
+  res.json(aboutUsData);
+});
+
 app.get('/messages', async (req, res) => {
-  // load all messages from database
   try {
-    const messages = await Message.find({})
+    const messages = await Message.find({});
     res.json({
       messages: messages,
       status: 'all good',
-    })
+    });
   } catch (err) {
-    console.error(err)
+    console.error(err);
     res.status(400).json({
       error: err,
       status: 'failed to retrieve messages from the database',
-    })
+    });
   }
-})
+});
 
-// a route to handle fetching a single message by its id
 app.get('/messages/:messageId', async (req, res) => {
-  // load all messages from database
   try {
-    const messages = await Message.find({ _id: req.params.messageId })
+    const messages = await Message.find({ _id: req.params.messageId });
     res.json({
       messages: messages,
       status: 'all good',
-    })
+    });
   } catch (err) {
-    console.error(err)
+    console.error(err);
     res.status(400).json({
       error: err,
       status: 'failed to retrieve messages from the database',
-    })
+    });
   }
-})
-// a route to handle logging out users
+});
+
 app.post('/messages/save', async (req, res) => {
-  // try to save the message to the database
   try {
     const message = await Message.create({
       name: req.body.name,
       message: req.body.message,
-    })
+    });
     return res.json({
-      message: message, // return the message we just saved
+      message: message,
       status: 'all good',
-    })
+    });
   } catch (err) {
-    console.error(err)
+    console.error(err);
     return res.status(400).json({
       error: err,
       status: 'failed to save the message to the database',
-    })
+    });
   }
-})
+});
 
-// export the express app we created to make it available to other modules
-module.exports = app // CommonJS export style!
+module.exports = app;
