fac1975ff5997811849403dbb39995337b2cf5a6
Eric Zhu <lz2348@nyu.edu>
2023-09-27 21:41:14 -0400

    11

--- front-end/src/AboutUs.js
+++ front-end/src/AboutUs.js
@@ -1,29 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
 const AboutUs = () => {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    // Fetch data from backend
-    axios.get('/Users/ericzhu/3-simple-mern-stack-app-Erikccc/back-end')
-      .then(response => {
-        setData(response.data);
-      })
-      .catch(error => {
-        console.error("Error fetching data:", error);
-      });
-  }, []);
-
-  if (!data) return <div>Loading...</div>;
-
-  return (
-    <div>
-      <h2>About Us</h2>
-      <p>{data.aboutText}</p>
-      <img src={data.imageUrl} alt="About us" />
-    </div>
-  );
+    return (
+        <div>
+            <h2>About Us</h2>
+            <p>Your content here...</p>
+            {/* You can add more content, images, etc. */}
+        </div>
+    );
 }
 
 export default AboutUs;
