a4c716ccd49f806ffa4c10d2f26185fc0ee84533
Eric Zhu <lz2348@nyu.edu>
2023-09-27 22:01:20 -0400

    11

--- back-end/server.js
+++ back-end/server.js
@@ -21,3 +21,14 @@ const close = () => {
 module.exports = {
   close: close,
 }
+
+// Sample data for "About Us" page
+const aboutUsData = {
+  aboutText: "Hello! I'm John Doe, a passionate web developer...",
+  imageUrl: "https://example.com/path/to/your/image.jpg"  // Replace with the actual URL of your image
+};
+
+// Endpoint to serve "About Us" data
+app.get('/api/about-us', (req, res) => {
+  res.json(aboutUsData);
+});
--- front-end/src/AboutUs.js
+++ front-end/src/AboutUs.js
@@ -1,21 +1,28 @@
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const AboutUs = () => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        axios.get('http://localhost:YOUR_BACKEND_PORT/api/about-us')  // Replace YOUR_BACKEND_PORT with the port your backend is running on
+        .then(response => {
+            setData(response.data);
+        })
+        .catch(error => {
+            console.error("Error fetching data:", error);
+        });
+    }, []);
+
+    if (!data) return <div>Loading...</div>;
+
     return (
         <div>
-            <nav>
-                <Link to="/">Home</Link> | {/* This is a link to the home page or main page */}
-                <Link to="/about-us">About Us</Link> {/* This link will reload the "About Us" page */}
-            </nav>
-            
             <h2>About Us</h2>
-            <p>Your content here...</p>
-            {/* You can add more content, images, etc. */}
+            <p>{data.aboutText}</p>
+            <img src={data.imageUrl} alt="About Us" />
         </div>
     );
 }
 
 export default AboutUs;
-
-
-
