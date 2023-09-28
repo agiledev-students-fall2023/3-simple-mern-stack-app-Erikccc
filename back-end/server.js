a36c8c013f622b48251764c02a3547c42947e570
Eric Zhu <lz2348@nyu.edu>
2023-09-27 22:43:23 -0400

    111

--- back-end/server.js
+++ back-end/server.js
@@ -16,19 +16,7 @@ const listener = server.listen(port, function () {
 const close = () => {
   listener.close()
 }
-const express = require('express');
-const app = express();
 
-// Sample data for "About Us" page
-const aboutUsData = {
-  aboutText: "My name is Eric Zhu and I am a senior in NYU. My major is computer science joint econ and I have been struggled four years studying these two hard major.",
-  imageUrl: "path/to/WechatIMG232.jpg" // Make sure this path points to where the image can be publicly accessed
-};
-
-// Endpoint to serve "About Us" data
-app.get('/api/about-us', (req, res) => {
-  res.json(aboutUsData);
-});
 // export the close function
 module.exports = {
   close: close,
