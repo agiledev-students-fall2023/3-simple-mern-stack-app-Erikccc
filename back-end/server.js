b0831631a730d704043e0abf07ee10f72cb79e19
Eric Zhu <lz2348@nyu.edu>
2023-09-27 22:06:40 -0400

    11

--- back-end/server.js
+++ back-end/server.js
@@ -1,26 +1,8 @@
 #!/usr/bin/env node
 
 // import the express app
-const server = require('./app')
-require('dotenv').config({ silent: true }) // load environmental variables from a hidden file named .env
-
-// which port to listen for HTTP(S) requests
-const port = process.env.PORT || 3000
-
-// call a function to start listening to the port
-const listener = server.listen(port, function () {
-  console.log(`Server running on port: ${port}`)
-})
-
-// a function to stop listening to the port
-const close = () => {
-  listener.close()
-}
-
-// export the close function
-module.exports = {
-  close: close,
-}
+const server = require('./app');
+require('dotenv').config({ silent: true }); // load environmental variables from a hidden file named .env
 
 // Sample data for "About Us" page
 const aboutUsData = {
@@ -29,6 +11,24 @@ const aboutUsData = {
 };
 
 // Endpoint to serve "About Us" data
-app.get('/api/about-us', (req, res) => {
+server.get('/api/about-us', (req, res) => {
   res.json(aboutUsData);
 });
+
+// which port to listen for HTTP(S) requests
+const port = process.env.PORT || 3000;
+
+// call a function to start listening to the port
+const listener = server.listen(port, function () {
+  console.log(`Server running on port: ${port}`);
+});
+
+// a function to stop listening to the port
+const close = () => {
+  listener.close();
+};
+
+// export the close function
+module.exports = {
+  close: close,
+};
